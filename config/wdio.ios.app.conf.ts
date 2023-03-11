d14765b7333aa490217976518ac91de11b2d73c8
kajaleprashant24 <kajaleprashant24@gmail.com>
2023-03-11 18:17:33 +0530

    Update wdio.ios.app.conf.ts

--- config/wdio.ios.app.conf.ts
+++ config/wdio.ios.app.conf.ts
@@ -19,14 +19,14 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // This is `appium:` for all Appium Capabilities which can be found here
         // http://appium.io/docs/en/writing-running-appium/caps/
-        "appium:deviceName": "iPhone 13",
-        "appium:platformVersion": "15.4",
+        "appium:deviceName": "iPhone 12",
+        "appium:platformVersion": "16.2",
         "appium:orientation": "PORTRAIT",
         "appium:automationName": "XCUITest",
         // The path to the app
         "appium:app": join(
             process.cwd(),
-            "./apps/iOS-Simulator-NativeDemoApp-0.4.0.app.zip"
+            "./apps/app.zip"
         ),
         "appium:newCommandTimeout": 240,
     },
