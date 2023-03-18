47e6bf64ea6504f34168f19d55d7c0f59274855b
kajaleprashant24 <kajaleprashant24@gmail.com>
2023-03-18 16:11:41 +0530

    Update app.image.gallery.spec.ts

--- tests/specs/app.image.gallery.spec.ts
+++ tests/specs/app.image.gallery.spec.ts
@@ -2,6 +2,7 @@ import HomeScreen from "../screenobjects/HomeScreen";
 
 describe("WebdriverIO and Appium, when interacting with a photo gallery app,", () => {
   const flag = driver.isAndroid ? true : false;
+  const searchInput = "Kittens";
   before(() => {
     console.log("Starting test");
   });
@@ -10,19 +11,50 @@ describe("WebdriverIO and Appium, when interacting with a photo gallery app,", (
     console.log("Test Completed!");
   });
 
-  it("should be able search images in photo gallery successfully", async () => {
+  beforeEach(async () => {
     await HomeScreen.waitForIsShown(flag);
 
-    // verift titke is displayed
+    // verift title is displayed
     const tiltleElement = await HomeScreen.title;
     expect(tiltleElement).toBeDisplayed();
+  });
 
+  it("should be able search images in photo gallery successfully", async () => {
     // search kittens in gallery
-    await HomeScreen.searchImage("Kittens");
+    await HomeScreen.searchImage(searchInput);
     const imageTitles = await HomeScreen.getSearchResultsTitles();
 
     // console log the text of images loaded
     // this can be replaced with test assertions
     console.log(imageTitles);
+  });
+
+  // Only for IOS
+  it("should clear input value after click on cancel", async () => {
+    // search kittens in gallery
+    await HomeScreen.searchImage(searchInput);
+    await HomeScreen.clearInput();
+    const searchTxt = await HomeScreen.getSearchInputValue();
+    expect(searchTxt).toEqual("Search here...");
+  });
+
+  it("search for empty string", async () => {
+    // search kittens in gallery
+    await HomeScreen.searchImage("");
+    await HomeScreen.clearInput();
+    const searchTxt = await HomeScreen.getSearchInputValue();
+    expect(searchTxt).toEqual("Search here...");
+  });
+
+  it("search for special chars", async () => {
+    // search kittens in gallery
+    await HomeScreen.searchImage("@");
+
+    const searchTxt = await HomeScreen.getSearchInputValue();
+    expect(searchTxt).toEqual("Search here...");
+  });
+
+  it("validate welcome message", async () => {
+    await HomeScreen.validateWelcomeMessage();
   });
 });
